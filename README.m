552b8e170a3054c615f4d3ae13af3a450a4f2a73
2000radp-star <2000radp@gmail.com>
2025-12-18 11:26:25 +0330

    Create README.m

---
+++ README.m
@@ -0,0 +1,16 @@
+#incLud < iostream >
+using namespace std;
+main()
+{
+int n1,n2,n3,n4;
+cout << " enter four integers " : ;
+cin >> n1 >> n2 >> n3 >> n4 ;
+int main = n1 ;
+if ( n2 < main )
+main = n2 ; 
+if ( n3 < main ) 
+if ( n4 < main )
+main = n4;
+cout << " their minimum is " : << main << endl;
+returno ;
+}
