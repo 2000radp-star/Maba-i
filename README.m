d9109c21bcd126ec42da4608f16a5a6c812adfa3
2000radp-star <2000radp@gmail.com>
2025-12-18 11:44:48 +0330

    Update README.m

--- README.m
+++ README.m
@@ -2,15 +2,10 @@
 using namespace std;
 main()
 {
-int n1,n2,n3,n4;
-cout << " enter four integers " : ;
-cin >> n1 >> n2 >> n3 >> n4 ;
-int main = n1 ;
-if ( n2 < main )
-main = n2 ; 
-if ( n3 < main ) 
-if ( n4 < main )
-main = n4;
-cout << " their minimum is " : << main << endl;
-returno ;
+int n , d ;
+cout << " enter two positive integer " : ;
+cin >> n >> d ;
+if ( n % d ) 
+cout << n << " is divisible by << d << endl ;
 }
+
