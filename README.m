e2b2198cf1c46bfe3acb46196bce673214cdcfb9
2000radp-star <2000radp@gmail.com>
2025-12-18 13:10:57 +0330

    Mabani

--- README.m
+++ README.m
@@ -2,9 +2,15 @@
 using namespace std;
 main()
 {
-int n , d ;
-cout >> " enter two positive integers " : ;
-cin >> n >> d ;
-if ( n % d ) 
-cout << n << " is divisible by " << endl ;
+int n1, n2 , n3 , n4 ;
+cout << " enter four integer " : ;
+cin >> n1 >> n2 >> n3 >> n4 ;
+int main = n1;
+if ( n2 < main )
+main = n2 ;
+if ( n3 < main)
+if ( n4 < main) 
+main = n4 ; 
+cout << " their minimum is " : <<  main << endl ;
+returno ;
 }
